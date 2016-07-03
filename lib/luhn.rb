d6d359df289a086fc1bb0d0ec6f052df4e6e3e73
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-07-03 22:51:04 +0000

    3 out of 4 tests are working, need to figure out why test 3 is failing

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,10 +1,17 @@
 module Luhn
   def self.is_valid?(number)
+    # transform bignum into array of integer digits
     array = number.to_s.split('').map(&:to_i)
-
+    # double every second digit
     array.map!.with_index { |n, i| i.even? ? n * 2 : n }
-    p array
+    # if doubled digit value is >= 10, subtract value by 9
+    array.map!.with_index { |n, i| (i.even? && n >= 10) ? n - 9 : n }
+    # sum all digits
+    sum = array.reduce(0) { |sum, n| sum + n }
+    # if the sum is divisible by 10, the number is valid
+    return (sum % 10 == 0) ? true : false
   end
 end
 
-Luhn.is_valid?(4194560385008504)
+testing = Luhn.is_valid?(4194560385008504)
+p testing
